c8c839229961f6e526d02b0fda3e0b58a738cd7d
creiht <cthier@gmail.com>
2026-04-30 11:28:07 -0500

    Add New Haven mines and small refactor

--- Projects/UOContent/Engines/Character Creation/CharacterCreation.cs
+++ Projects/UOContent/Engines/Character Creation/CharacterCreation.cs
@@ -87,7 +87,6 @@ public static partial class CharacterCreation
 
     public static readonly CityInfo[] FeluccaStartingCities =
     [
-        new("New Haven", "The Bountiful Harvest Inn", 1150168, 3503, 2574, 14, Map.Felucca),
         new("Yew", "The Empath Abbey", 1075072, 633, 858, 0, Map.Felucca),
         new("Minoc", "The Barnacle", 1075073, 2476, 413, 15, Map.Felucca),
         new("Britain", "Sweet Dreams Inn", 1075074, 1496, 1628, 10, Map.Felucca),
@@ -122,6 +121,20 @@ public static partial class CharacterCreation
         new("Royal City", "Royal City Inn", 1150169, 738, 3486, -19, Map.TerMur)
     ];
 
+    public static readonly CityInfo[] SolusStartingCities =
+    [
+        new("New Haven", "The Bountiful Harvest Inn", 1150168, 3503, 2574, 14, Map.Felucca),
+        new("Yew", "The Empath Abbey", 1075072, 633, 858, 0, Map.Felucca),
+        new("Minoc", "The Barnacle", 1075073, 2476, 413, 15, Map.Felucca),
+        new("Britain", "Sweet Dreams Inn", 1075074, 1496, 1628, 10, Map.Felucca),
+        new("Moonglow", "The Scholars Inn", 1075075, 4408, 1168, 0, Map.Felucca),
+        new("Trinsic", "The Traveler's Inn", 1075076, 1845, 2745, 0, Map.Felucca),
+        new("Magincia", "The Great Horns Tavern", 1075077, 3734, 2222, 20, Map.Felucca),
+        new("Jhelom", "The Mercenary Inn", 1075078, 1374, 3826, 0, Map.Felucca),
+        new("Skara Brae", "The Falconer's Inn", 1075079, 618, 2234, 0, Map.Felucca),
+        new("Vesper", "The Ironwood Inn", 1075080, 2771, 976, 0, Map.Felucca)
+    ];
+
     private static CityInfo[] _availableStartingCities;
 
     public static CityInfo[] GetStartingCities() =>
@@ -134,6 +147,9 @@ public static partial class CharacterCreation
         var trammelAvailable = availableMaps.Includes(MapSelectionFlags.Trammel);
         var terMerAvailable = availableMaps.Includes(MapSelectionFlags.TerMur);
 
+        // Return Solus starting cities
+        return SolusStartingCities;
+        
         if (trammelAvailable)
         {
             if (pre6000ClientSupport)
